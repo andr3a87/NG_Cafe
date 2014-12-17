3c7d9bd2671286b31c016096a8a69303baa28ff1
DaveHC <davidessj87@hotmail.com>
2014-12-17 16:27:43 +0100

    Aggiunta la metro di londra con euristica euclidea e costi non unitari

---
+++ Prolog/tube_euclide.pl
@@ -0,0 +1,141 @@
+% stato: [at(Stazione), Location]
+% Location può essere in(NomeLinea, Dir) o
+%  'ground' se l'agente non è su nessun treno
+% Dir può esere 0 o 1
+
+
+% Azioni:
+%  sali(Linea, Dir)
+%  scendi(Stazione)
+%  vai(Linea, Dir, StazionePartenza, StazioneArrivo)
+
+:-dynamic(f_val/1). f_val(0).
+:-dynamic(h_val/1). h_val(0).
+
+applicabile(sali(Linea,Dir),[at(Stazione),ground]):-
+        fermata(Stazione,Linea), member(Dir,[0,1]).
+applicabile(scendi(Stazione),[at(Stazione),in(_,_)]).
+applicabile(vai(Linea,Dir,SP,SA),[at(SP),in(Linea,Dir)]):-
+        tratta(Linea,Dir,SP,SA).
+
+trasforma(sali(Linea,Dir),[at(Stazione),ground],[at(Stazione),in(Linea,Dir)]).
+trasforma(scendi(Stazione),[at(Stazione),in(_,_)],[at(Stazione),ground]).
+trasforma(vai(Linea,Dir,SP,SA),[at(SP),in(Linea,Dir)],[at(SA),in(Linea,Dir)]):-
+        tratta(Linea,Dir,SP,SA).
+        
+uguale(S,S).
+
+% percorso(Linea, Dir, ListaFermate)
+
+percorso(piccadilly,0,['Kings Cross','Holborn','Covent Garden',
+        'Leicester Square','Piccadilly Circus','Green Park','South Kensington',
+        'Gloucester Road','Earls Court']).
+percorso(jubilee,0,['Baker Street','Bond Street','Green Park',   
+        'Westminster','Waterloo','London Bridge']).
+percorso(central,0,['Notting Hill Gate','Bond Street','Oxford Circus',
+        'Tottenham Court Road','Holborn','Bank']).
+percorso(victoria,0,['Kings Cross','Euston','Warren Street',
+        'Oxford Circus','Green Park','Victoria']).
+percorso(bakerloo,0,['Paddington','Baker Street','Oxford Circus',
+        'Piccadilly Circus','Embankment','Waterloo']).
+percorso(circle,0,['Embankment','Westminster','Victoria','South Kensington',
+        'Gloucester Road','Notting Hill Gate','Bayswater','Paddington',
+        'Baker Street','Kings Cross']).
+        
+percorso(Linea,1,LR):- percorso(Linea,0,L), reverse(L,LR).
+
+
+% tratta(NomeLinea, Dir, StazionePartenza, StazioneArrivo)
+
+tratta(Linea,Dir,SP,SA):- percorso(Linea,Dir,LF), member_pair(SP,SA,LF).
+
+member_pair(X,Y,[X,Y|_]).
+member_pair(X,Y,[_,Z|Rest]):- member_pair(X,Y,[Z|Rest]).
+
+
+% stazione(Stazione, Coord1, Coord2)
+
+stazione('Baker Street',4.5,5.6).
+stazione('Bank',12,4).
+stazione('Bayswater',1,3.7).
+stazione('Bond Street',5.4,4.1).
+stazione('Covent Garden',8,4).
+stazione('Earls Court',0,0).
+stazione('Embankment',8.2,3).
+stazione('Euston',7.1,6.6).
+stazione('Gloucester Road',1.6,0.6).
+stazione('Green Park',6,2.8).
+stazione('Holborn',8.6,4.8).
+stazione('Kings Cross',8.2,7.1).
+stazione('Leicester Square',7.6,3.6).
+stazione('London Bridge',0,0).
+stazione('Notting Hill Gate',0,3.2).
+stazione('Oxford Circus',6.2,4.3).
+stazione('Paddington',2.4,4.2).
+stazione('Piccadilly Circus',7,3.3).
+stazione('South Kensington',2.6,0.5).
+stazione('Tottenham Court Road',7.4,4.5).
+stazione('Victoria',5.8,1).
+stazione('Warren Street',6.5,6).
+stazione('Waterloo',9.2,2.4).
+stazione('Westminster',8,1.8).
+
+
+fermata(Stazione,Linea):- percorso(Linea,0,P), member(Stazione,P).
+
+
+iniziale([at('Bayswater'),ground]).
+
+finale([at('Covent Garden'),ground]).
+
+calcolo_euristica([at(Stazione1),_],[at(Stazione2),_]) :-
+        stazione(Stazione1, R, C),
+        stazione(Stazione2, R1, C1),
+        X is (R - R1)^2,
+        Y is (C - C1)^2,
+        abs(X, Xabs),
+        abs(Y, Yabs),
+        H is Xabs + Yabs,
+        F is sqrt(H),
+        retract(f_val(_)),
+        assert(f_val(F)),
+        retract(h_val(_)),
+        assert(h_val(H)).
+
+ric_astar([nodo(_,_, S, Lista_Az)|_],_, Lista_Az) :- finale(S), !.
+ric_astar([nodo(Fcost, Gcost, S, Lista_Az)| R_lista_open], Closed, Lista_Ris) :-
+        member(S, Closed) ->
+                ric_astar(R_lista_open, Closed, Lista_Ris);
+        open_node(nodo(Fcost, Gcost, S, Lista_Az), Lista_children),
+        ord_union(Lista_children, R_lista_open, Nuova_open),
+        ric_astar(Nuova_open,[S|Closed], Lista_Ris).
+
+        
+open_node(nodo(Fcost, Gcost, S, Lista_Az), Lista_childern) :-
+        findall(Az, applicabile(Az,S), Az_applicabili),
+        best_node(nodo(Fcost, Gcost, S, Lista_Az), Az_applicabili, Lista_childern).
+
+best_node(_,[],[]).
+best_node(nodo(Fcost, Gcost, S, Lista_Az), [Az|R_az], Lista_children) :-
+        finale(Goal),        
+        trasforma(Az, S, Nuovo_S),
+        append(Lista_Az, [Az], Nuova_lista_az),
+        best_node(nodo(Fcost, Gcost, S, Lista_Az), R_az, Old_children),
+        calcola_G(Az, Gcost, G1),
+        calcolo_euristica(Nuovo_S, Goal),
+        f_val(F),
+        ord_add_element(Old_children, nodo(F, G1, Nuovo_S, Nuova_lista_az), Lista_children).
+        
+calcola_G(sali(_,_), Gcost, G1) :-
+        G1 is Gcost + 10.
+        
+calcola_G(scendi(_), Gcost, G1) :-
+        G1 is Gcost + 10.
+        
+calcola_G(vai(_,_,_,_), Gcost, G1) :-
+        G1 is Gcost + 5.
+
+astar :-
+        iniziale(S),
+        ric_astar([nodo(0, 0, S, [])], [], Ris),
+        write(Ris).
